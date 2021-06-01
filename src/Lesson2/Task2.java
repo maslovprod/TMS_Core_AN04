2c485661c38de8c5ad9d71e4b8be4398ec628689
Maslov Ilya <maslovpenza92@gmail.com>
2021-06-01 21:57:38 +0300

    Lesson 2. Maslov Ilya. Homework

--- src/Lesson2/Task2.java
+++ src/Lesson2/Task2.java
@@ -27,63 +27,63 @@ public class Task2 {
     }
 
     private static String getZodiacName(LocalDate localInputDate) {
-        LocalDate DateStart;
-        LocalDate DateEnd;
+        LocalDate dateStart;
+        LocalDate dateEnd;
         int localYear = localInputDate.getYear();
 
-        DateStart = LocalDate.of(localYear, 12, 21);
-        DateEnd = LocalDate.of(localYear, 1, 20);
-        if (localInputDate.isBefore(DateEnd) || localInputDate.isAfter(DateStart)) {
+        dateStart = LocalDate.of(localYear, 12, 21);
+        dateEnd = LocalDate.of(localYear, 1, 20);
+        if (localInputDate.isBefore(dateEnd) || localInputDate.isAfter(dateStart)) {
             return "Козерог";
         }
-        DateStart = LocalDate.of(localYear, 1, 19);
-        DateEnd = LocalDate.of(localYear, 2, 19);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 1, 19);
+        dateEnd = LocalDate.of(localYear, 2, 19);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Водолей";
         }
-        DateStart = LocalDate.of(localYear, 2, 18);
-        DateEnd = LocalDate.of(localYear, 3, 21);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 2, 18);
+        dateEnd = LocalDate.of(localYear, 3, 21);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Рыбы";
         }
-        DateStart = LocalDate.of(localYear, 3, 20);
-        DateEnd = LocalDate.of(localYear, 4, 21);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 3, 20);
+        dateEnd = LocalDate.of(localYear, 4, 21);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Овен";
         }
-        DateStart = LocalDate.of(localYear, 4, 20);
-        DateEnd = LocalDate.of(localYear, 5, 22);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 4, 20);
+        dateEnd = LocalDate.of(localYear, 5, 22);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Телец";
         }
-        DateStart = LocalDate.of(localYear, 5, 20);
-        DateEnd = LocalDate.of(localYear, 6, 21);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 5, 20);
+        dateEnd = LocalDate.of(localYear, 6, 21);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Близнецы";
         }
-        DateStart = LocalDate.of(localYear, 6, 20);
-        DateEnd = LocalDate.of(localYear, 7, 23);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 6, 20);
+        dateEnd = LocalDate.of(localYear, 7, 23);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Рак";
         }
-        DateStart = LocalDate.of(localYear, 7, 22);
-        DateEnd = LocalDate.of(localYear, 8, 23);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 7, 22);
+        dateEnd = LocalDate.of(localYear, 8, 23);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Лев";
         }
-        DateStart = LocalDate.of(localYear, 8, 22);
-        DateEnd = LocalDate.of(localYear, 9, 24);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 8, 22);
+        dateEnd = LocalDate.of(localYear, 9, 24);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Дева";
         }
-        DateStart = LocalDate.of(localYear, 9, 23);
-        DateEnd = LocalDate.of(localYear, 10, 24);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 9, 23);
+        dateEnd = LocalDate.of(localYear, 10, 24);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Весы";
         }
-        DateStart = LocalDate.of(localYear, 10, 23);
-        DateEnd = LocalDate.of(localYear, 11, 22);
-        if (localInputDate.isAfter(DateStart) && localInputDate.isBefore(DateEnd)) {
+        dateStart = LocalDate.of(localYear, 10, 23);
+        dateEnd = LocalDate.of(localYear, 11, 22);
+        if (localInputDate.isAfter(dateStart) && localInputDate.isBefore(dateEnd)) {
             return "Скорпион";
         }
         return "";
